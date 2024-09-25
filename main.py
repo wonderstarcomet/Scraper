ff88e9a2680d0c104487ac29d25e91fb5aebcec5
nerdybunny <nerdragecorp@gmail.com>
2024-09-25 17:51:56 -0600

    Fixes

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import re
 import json
 
-# Step 1: Get the page content
+# basic setup
 URL = "https://devnetproject.netlify.app/"
 page = requests.get(URL)
 soup = BeautifulSoup(page.content, "html.parser")
@@ -13,11 +13,13 @@ scripts = soup.find_all("script")
 #print(scripts)
 realtors = None
 
+# find the full dict of realtors
 for s in scripts:
     if s.string and "const realtors" in s.string:
         realtors = s.string
         break
 
+# do insane regex magic to actually find the data we're looking for
 realtorArray = re.search(r'const realtors = (\[.*?\]);', realtors, re.DOTALL)
 
 data = realtorArray.group(1)
@@ -25,7 +27,8 @@ data = realtorArray.group(1)
 json_data = re.sub(r'(\w+):', r'"\1":', data)
 json_data = json_data.replace("'", '"')
 
+# convert the json to a dict so it's easier to work with
 realtorDict = json.loads(json_data)
 
 for key in realtorDict:
-    
+    print(key)
