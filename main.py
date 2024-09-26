088534600fe727656bfd29aaf9d015a9d3b49146
Mateo-mme <estrada3mm@gmail.com>
2024-09-25 18:18:08 -0600

    Update main.py
    added new code

--- main.py
+++ main.py
@@ -1,34 +1,15 @@
-import requests
-from bs4 import BeautifulSoup
-import re
+import requests 
 import json
-
-# basic setup
+from bs4 import BeautifulSoup
 URL = "https://devnetproject.netlify.app/"
 page = requests.get(URL)
+
 soup = BeautifulSoup(page.content, "html.parser")
 
-scripts = soup.find_all("script")
-
-#print(scripts)
-realtors = None
-
-# find the full dict of realtors
-for s in scripts:
-    if s.string and "const realtors" in s.string:
-        realtors = s.string
-        break
-
-# do insane regex magic to actually find the data we're looking for
-realtorArray = re.search(r'const realtors = (\[.*?\]);', realtors, re.DOTALL)
-
-data = realtorArray.group(1)
-
-json_data = re.sub(r'(\w+):', r'"\1":', data)
-json_data = json_data.replace("'", '"')
-
-# convert the json to a dict so it's easier to work with
-realtorDict = json.loads(json_data)
-
-for key in realtorDict:
-    print(key)
+table = soup.find("body")
+body = table.find("script").text.replace(" ","").replace("name", '"name"').replace("phone", '"phone"').replace("specialization",'"speciaization"').replace("experience",'"experience"')[15:-331]
+phone = json.loads(body)
+name = input().replace(" ","")
+for table in phone:
+    if table["name"]==name:
+        print(f"name: {table["name"]}, phone: {table["phone"]}")
